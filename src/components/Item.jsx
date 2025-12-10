d8dc55f06f6dfb0b031bc2a3c1662b8de4de4ad9
DavidMoravec <David.Moravec2@seznam.cz>
2025-12-10 19:19:32 +0100

    Update product dataset + implement load more feature + cart fixes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,32 +1,46 @@
 import { Link } from "react-router-dom";
 import data from "../data/data";
 import "./Item.css";
+import { useState } from "react";
 
 const Item = (props) => {
+  const [visibleCount, setVisibleCount] = useState(10);
+  const loadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 10);
+  };
   return (
-    <section className="section">
-      {data.map((item) => {
-        return (
-          <Link to={`/item/${item.id}`} key={item.id}>
-            <div className="oneItem">
-              <h2 className="itemName">{item.name}</h2>
-              <img src={item.image} alt="" className="itemImg" />
-              <p className="itemDesc">{item.description}</p>
-              <strong className="itemPrice">{item.price},-</strong>
-              <button
-                className="buyBtn"
-                onClick={(e) => {
-                  e.preventDefault();
-                  props.addToCart(item);
-                }}
-              >
-                Koupit
-              </button>
-            </div>
-          </Link>
-        );
-      })}
-    </section>
+    <>
+      <section className="section">
+        {data.slice(0, visibleCount).map((item) => {
+          return (
+            <Link to={`/item/${item.id}`} key={item.id}>
+              <div className="oneItem">
+                <h2 className="itemName">{item.name}</h2>
+                <img src={item.image} alt="" className="itemImg" />
+                <p className="itemDesc">{item.description}</p>
+                <strong className="itemPrice">{item.price},-</strong>
+                <button
+                  className="buyBtn"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    props.addToCart(item);
+                  }}
+                >
+                  Koupit
+                </button>
+              </div>
+            </Link>
+          );
+        })}
+      </section>
+      {visibleCount < data.length && (
+        <div className="loadMoreWrapper">
+          <button onClick={loadMore} className="loadMoreBtn">
+            Načíst více
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
